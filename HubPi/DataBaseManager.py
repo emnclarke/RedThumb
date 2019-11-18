89cbb9bd8549ab708a5a9ff1750eec9c840f6daf
Alden Ng <alden_722@hotmail.com>
2019-11-18 15:08:14 -0500

    Update DataBaseManager.py

--- HubPi/DataBaseManager.py
+++ HubPi/DataBaseManager.py
@@ -14,13 +14,29 @@ class DataBaseManager:
         self.__connect = sqlite3.connect('plants.db')
         self.__cursor = self.__connect.cursor()
 
+    # creates the table for the plant data if it does not exist
     def create_table(self):
         self.__cursor.execute(
             "CREATE TABLE IF NOT EXISTS plantData(temperature REAL, humidity REAL, soil_moisture REAL, sunlight INTEGER)")
 
+    # adds the plant data to plant data table
     def add_plant_data(self, temperature, humidity, soil_moisture, sunlight):
         # unix = int(time.time())
         # date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+
+        self.__.cursor.execute(
+            "INSERT INTO plantData(temperature, humidity, soil_moisture, sunlight) VALUES (?, ?, ?, ?)",
+            (temperature, humidity, soil_moisture, sunlight))
+
+        self.__connect.commit()
+
+    # creates a new plant type into the database. A string array is passed by the containing the plant information
+    # plant_info is an array of strings containing the information about the plant type
+    def create_plant_type(self, plant_info):
+        temperature = plant_info[0]
+        humidity = plant_info[1]
+        soil_moisture = plant_info[2]
+        sunlight = plant_info[3]
 
         self.__.cursor.execute(
             "INSERT INTO plantData(temperature, humidity, soil_moisture, sunlight) VALUES (?, ?, ?, ?)",
@@ -33,6 +49,14 @@ if __name__ == "__main__":
     data_base_manager = DataBaseManager("random string")
     data_base_manager.create_table()
 
+    # temporary values
+    temperature = 25
+    humidity = 10
+    soil_moisture = 10
+    sunlight = True
+
     while True:
-        data_base_manager.add_plant_data()
+
+        # will receive data from the pot pi
+        data_base_manager.add_plant_data(temperature, humidity, soil_moisture, sunlight)
         time.sleep(1)
