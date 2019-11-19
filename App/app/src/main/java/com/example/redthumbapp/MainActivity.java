fbb8febb98ccb37ee42b7e0f253c0f2599c79876
Steve W <stevewicklund@gmail.com>
2019-11-19 11:07:57 -0500

    Migrated to AndriodX
    >This is required to use a card layout, which I belive is ideal for our GUI.
    >I have also made some minor layout changes.

--- App/app/src/main/java/com/example/redthumbapp/MainActivity.java
+++ App/app/src/main/java/com/example/redthumbapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.redthumbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.settings:
+                //TODO:Navigate to settings screen
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
 }
