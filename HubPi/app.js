a92a18e753f949cf51c2d110a488c5408757c9c8
Emily Clarke <emily.clarke3@carleton.ca>
2019-12-06 16:58:05 -0500

    comments

--- HubPi/app.js
+++ HubPi/app.js
@@ -1,35 +1,36 @@
+// Setting up server settings
 const http = require('http');
-
 const PORT = 3000;
-
 const hostname = '192.168.43.85';
-
 var url = require('url');
 
+// Configuring server response
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  var q = url.parse(req.url, true).query;
+  var q = url.parse(req.url, true).query; // Parsing url to get args
   console.log(req.url)
   console.log(q.request);
   
   var spawn = require("child_process").spawn;
+  // Executing python connector. q holds the arguments passed in from the url
   var process = spawn('python',["./nodeConnector.py", q.request, q.arg1, q.arg2, q.arg3, q.arg4, q.arg5, q.arg6, q.arg7, q.arg8]);
 
   var response = ""
 
+  // Listens for output from the python script
   process.stdout.on('data', function(data) {
+    // adds python output to response
     response += data.toString();
-    // res.end(data.toString());
+    // when end is reached (signified by "end" at end of python output), sends response to
+    // android app
     if (data.toString().split(" ").slice(-1)[0].includes("end") == true) {
-      // console.log(response);
       res.end(response);
     }
   } )
-
-  // res.end(response);
 });
 
+// Creating server listner
 server.listen(PORT, hostname, () => {
   console.log(`Server running on port ${PORT}.`);
 });
